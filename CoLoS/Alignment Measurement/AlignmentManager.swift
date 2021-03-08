98649040593c1659063f416273fd2174f53ca205
Tim Jaeger <timjae@schuldorf.de>
2021-03-08 17:54:16 +0100

    Kommentare AlignmentManager.swift

--- CoLoS/Alignment Measurement/AlignmentManager.swift
+++ CoLoS/Alignment Measurement/AlignmentManager.swift
@@ -10,7 +10,7 @@ import CoreMotion
 
 class AlignmentManager {
     
-    let manager: CMMotionManager
+    let manager: CMMotionManager // Objekt, welches die Sensoren auslesen kann
     
     var elevation = 0.0
     var azimut = 0.0
@@ -19,12 +19,12 @@ class AlignmentManager {
         
         manager = CMMotionManager()
         
-        if manager.isDeviceMotionAvailable {
+        if manager.isDeviceMotionAvailable { // überprüft, ob Sensoren verfügbar sind
             
             let measurementQueue = OperationQueue()
             measurementQueue.name = "com.timjaeger.measurementQueue"
             
-            manager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xMagneticNorthZVertical, to: measurementQueue, withHandler: deviceMotionHasUpdated)
+            manager.startDeviceMotionUpdates(using: CMAttitudeReferenceFrame.xMagneticNorthZVertical, to: measurementQueue, withHandler: deviceMotionHasUpdated) // Sensorwerte werden nun ausgelesen.
         }
         else {
             
@@ -32,9 +32,11 @@ class AlignmentManager {
         }
     }
     
-    func deviceMotionHasUpdated(data: CMDeviceMotion?, error: Error?) {
+    func deviceMotionHasUpdated(data: CMDeviceMotion?, error: Error?) { // wird ausgeführt, sobalt neue Werte verfügbar sind
         
         if let data = data {
+            
+            // Berechnung des Azimuts und der Elevation
             
             elevation = (Double.pi / 2.0) - data.attitude.pitch
             
