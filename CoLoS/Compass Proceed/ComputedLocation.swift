026559a9199fccc58d6777550b62ab7173009965
mprchub <62764211+mprchub@users.noreply.github.com>
2021-03-10 17:13:57 +0100

    Kommentare hinzugefügt

--- CoLoS/Compass Proceed/ComputedLocation.swift
+++ CoLoS/Compass Proceed/ComputedLocation.swift
@@ -10,12 +10,14 @@ import CoreLocation
 
 struct ComputedLocation {
     
+    let coordinate: CLLocationCoordinate2D
+    
+    // CoLoS arbeitet nur mit Radians; hier wird der Standort, die "ComputedLocation" in Grad° umgerechnet
     init(_ radiansCoordinate: RadiansCoordinate) {
         
         coordinate = CLLocationCoordinate2D(latitude: toDegrees(radiansCoordinate.latitude), longitude: toDegrees(radiansCoordinate.longitude))
     }
     
-    let coordinate: CLLocationCoordinate2D
 }
 
 typealias RadiansCoordinate = CLLocationCoordinate2D
