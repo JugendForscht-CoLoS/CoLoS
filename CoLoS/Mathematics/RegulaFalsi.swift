6233fff88b5ca0b8fe16116aee96923da0587ee5
mprchub <62764211+mprchub@users.noreply.github.com>
2021-03-10 18:12:32 +0100

    Kommentare hinzugefügt

--- CoLoS/Mathematics/RegulaFalsi.swift
+++ CoLoS/Mathematics/RegulaFalsi.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-
+// Methode zum Lösen von Nullstellen mit Regula Falsi
 func solve(xStart: Double, f: (Double) -> Double) throws -> Double {
     
     let e: Double = pow(10, -16) //accuracy of Double
@@ -15,20 +15,20 @@ func solve(xStart: Double, f: (Double) -> Double) throws -> Double {
     var b = xStart + pow(10, -15)
     
     var i = 0
-    
+    // um das bestmögliche Ergebnis für den Computer zu erhalten, wird die Schleife bei der höchsten Genauigkeit für ein Double gestoppt
     while abs(a - b) > e {
-        
+        // das Verfahren stoppt, wenn eine Sekante horizontal, also parallel zur x achse liegt
         if f(a) == f(b) {
             
             throw RegulaFalsiError.horizontalSecant
         }
-        
+        // der eigentliche Iterationsschritt
         let aAlt = a
         a = aAlt - f(aAlt) * ((b - aAlt) / (f(b) - f(aAlt)))
         b = aAlt
         
         i += 1
-        
+        // das Verfahren stoppt nach 100 Iterationen
         if i == 100 {
             
             throw RegulaFalsiError.notFoundSolution
